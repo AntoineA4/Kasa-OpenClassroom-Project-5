a930164cb323f64629a6c7c75da990a64b64a1a4
Antoine Gadrat <antoinegadrat@hotmail.fr>
2024-07-31 15:33:31 +0900

    feat: finalized error page + change logo color for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import  "../styles/components/Footer.scss"
-import LOGO_PHONE from '../assets/LOGO-PHONE.png'
+import LOGO_white from '../assets/LOGO-white.png'
 
 function Footer() {
     return (
         <div className="footer-container">
-            <img src={LOGO_PHONE} alt='Logo Kasa' className="footer-logo" />
+            <img src={LOGO_white} alt='Logo Kasa' className="footer-logo" />
             <p className="footer-text">© 2020 Kasa. All rights reserved</p>
         </div>
     )
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -4,8 +4,8 @@ import '../styles/Pages/Error.scss'
 function Error() {
     return (
         <div className="errorContainer">
-            <p className="404">404</p>
-            <p className="errorMessage">Oups! La pageque vous demandez n'existe pas.</p>
+            <p className="errorNumber">404</p>
+            <p className="errorMessage">Oups! La page que vous demandez n'existe pas.</p>
             <Link to="/" className="homeLink">Retourner sur la page d'acceuil</Link>
         </div>
     )
